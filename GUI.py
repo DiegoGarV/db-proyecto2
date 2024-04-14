27c6e45f50b83896a9d7a7d757a3444930cb34b6
Czar272 <110710742+Czar272@users.noreply.github.com>
2024-04-14 15:05:34 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -15,7 +15,7 @@ main_menu_window.title("Menú Principal")
 main_menu_window.geometry("1000x500")
 main_menu_window.withdraw()
 
-def Signin():
+def Signin(): #Aqui hay que hacer que guarde en la base de datos los usuarios y sus contraseñas
     signin.deiconify()
     window.withdraw()
     username = entry_usuario_sn.get()
@@ -24,7 +24,7 @@ def Signin():
     btn_Signin = tk.Button(signin, text = "Registrarse", command=Signin)
     btn_Signin.pack(pady = 5,padx=5)
     
-def login():
+def login(): #Aqui tiene que jalar los datos de la base de datos
     username = entry_username_w.get()
     password = entry_password_w.get()
     
@@ -33,7 +33,7 @@ def login():
     else:
         messagebox.showerror("Error de inicio de sesión", "Usuario o contraseña incorrectos")
         
-def open_main_menu():
+def open_main_menu(): #Me falta meterle para ponerle todas las opciones a otras pantallas
     window.withdraw()
     main_menu_window.deiconify()
     
@@ -50,7 +50,7 @@ def return_to_login():
     main_menu_window.withdraw()
     window.deiconify()
 
-
+#Algunos labels y botones
 label_username_w = tk.Label(window, text="Nombre de usuario:")
 label_username_w.pack(pady=5)
 entry_username_w = tk.Entry(window)
